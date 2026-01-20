6e9b24bec93b38e33c51f10a045795e39cad0aaf
Molly Olson <molly.olson@student.centralia.edu>
2026-01-20 10:10:07 -0800

    added some Validation

--- AreaCalculator/Form1.cs
+++ AreaCalculator/Form1.cs
@@ -6,21 +6,27 @@ namespace AreaCalculator
         {
             InitializeComponent();
         }
-
         private void frmArea_Load(object sender, EventArgs e)
         {
             //load event, fires when form loads
-            //change the text on the txtWidth to 314
-            txtWidth.Text = "314";
+            
         }
 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
+           
+            if (!Validators.IsMinimumLength(txtWidth.Text, 1) ||
+                !Validators.IsMinimumLength(txtHeight.Text, 1)
+                )
+            {               
+                return;
+            }
             //calculate the width x the height and change the total lable to match.
             int total = (int.Parse(txtWidth.Text)) * (int.Parse(txtHeight.Text));
 
             //change total label to match.
             lblTotal.Text = "Total: " + total.ToString();
+            
 
         }
     }
---
+++ AreaCalculator/Validators.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculator
+{
+    public static class Validators
+    {
+        public static bool IsMinimumLength(string value, int min)
+        {
+            return value.Length >= min;
+        }
+    }
+}
