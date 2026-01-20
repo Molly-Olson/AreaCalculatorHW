d9bf6c876ebd17599c8530cec15233650915b197
Molly Olson <molly.olson@student.centralia.edu>
2026-01-20 10:10:27 -0800

    validators

--- AreaCalculator/Form1.cs
+++ AreaCalculator/Form1.cs
@@ -1,3 +1,5 @@
+using System.CodeDom.Compiler;
+
 namespace AreaCalculator
 {
     public partial class frmArea : Form
@@ -11,14 +13,38 @@ namespace AreaCalculator
             //load event, fires when form loads
             
         }
-
         private void btnCalculate_Click(object sender, EventArgs e)
         {
            
-            if (!Validators.IsMinimumLength(txtWidth.Text, 1) ||
-                !Validators.IsMinimumLength(txtHeight.Text, 1)
+            //if (!Validators.IsMinimumLength(txtWidth.Text, 1) ||
+            //    !Validators.IsMinimumLength(txtHeight.Text, 1)
+            //    )
+            //{
+            //   var result = MessageBox.Show(
+            //       "Your values are not valid. Please try again.", 
+            //       "Error.", 
+            //       MessageBoxButtons.OKCancel
+            //       );
+
+            //    if (result == DialogResult.OK)
+            //    {
+            //        MessageBox.Show("I am pleased you are ok with it.");
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("Cancelling...");
+            //    }
+            //        return;
+            //}
+            if (ValidateFeild(txtWidth.Text, "The Width Feild is too short.")
+            ////    ValidateFeild(txtHeight.Text, "The Height Field is too short.")
                 )
-            {               
+            {
+                return;
+            }
+            // this is splitting the command however we can use DeMorgan's Law instead
+            if (ValidateFeild(txtHeight.Text, "The Height Field is too short."))
+            {
                 return;
             }
             //calculate the width x the height and change the total lable to match.
@@ -28,6 +54,20 @@ namespace AreaCalculator
             lblTotal.Text = "Total: " + total.ToString();
             
 
+        }
+        private bool ValidateFeild(string feild, string errorMessage)
+        {
+            var result = !Validators.IsMinimumLength(feild, 1);
+
+            if (result)
+            {
+                var dialogResult = MessageBox.Show(
+                    errorMessage,
+                    "Error."
+                    );
+            }
+
+                return result;
         }
     }
 }
